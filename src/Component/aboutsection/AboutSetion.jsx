528d250bcfc7761fa4ba9af3804da015570c03c8
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-06-27 10:25:20 +0600

    update

--- src/Component/aboutsection/AboutSetion.jsx
+++ src/Component/aboutsection/AboutSetion.jsx
@@ -9,9 +9,9 @@ const AboutSetion = () => {
             <p className='md:text-xl text-lg text-center md:text-start font-semibold md:mb-10 mb-8'>My About</p>
            <div className='flex md:flex-row-reverse md:gap-x-20 gap-x-0 gap-y-8 md:gap-y-0 flex-col-reverse'>
            <div className=' space-y-3 px-3 flex-1 md:pt-8 mt-0  '>
-                            <p className='text-2xl font-semibold'>I`m <span className='text-green-600'>Junayet Shiblu</span>, a Web Developer</p>
-                            <p className='text-lg text-[#a2a8a8]'> Passionate MERN stack developer who loves making user-friendly Website . I enjoy working with others to turn tricky ideas into smart solutions. I`m dedicated and a good team player, ready to bring my skills to your projects. Excited about creating cool web stuff together!</p>
-                            <p className='text-base text-[#6e7272]'>
+                            <p className='text-base md:text-lg font-semibold'>I`m <span className='text-green-600'>Junayet Shiblu</span>, a Web Developer</p>
+                            <p className='text-sm md:text-base text-[#a2a8a8]'> Passionate MERN stack developer who loves making user-friendly Website . I enjoy working with others to turn tricky ideas into smart solutions. I`m dedicated and a good team player, ready to bring my skills to your projects. Excited about creating cool web stuff together!</p>
+                            <p className='text-sm md:text-base text-[#6e7272]'>
                                 Also I am studying computer science and engineering at National Institute of Textile Engineering and Research so my love for coding works differently.
                             </p> 
                             <div className='flex items-center md:gap-5 gap-5'>
